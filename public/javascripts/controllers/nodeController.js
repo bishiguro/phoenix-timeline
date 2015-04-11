5dec84d175208f38ebd44999997ae68d9aeffa1c
Haley <haley.pelletier@students.olin.edu>
2015-04-11 14:47:08 -0400

    change location of node form

--- public/javascripts/controllers/nodeController.js
+++ public/javascripts/controllers/nodeController.js
@@ -3,14 +3,19 @@ var app = angular.module('projectManager', []).controller('nodeController', ['$s
     $scope.summary = '';
     $scope.description = '';
     $scope.dueDate = '';
-    $scope.show = false;
+    $scope.visible = false;
 
     $scope.addNode = function() {
-        $scope.show = !$scope.show;
+        $scope.visible = !$scope.visible;
         $http.post('/node/add',{sum:$scope.summary,desc:$scope.description,due:$scope.dueDate}).success(function(data,status,headers,config) {
                 $("#node-container").prepend("<button class='node' id='"+data.id+"'></button>");
             }).error(console.error);
     };
+
+    $scope.show = function() {
+        $scope.visible = !$scope.visible;
+    };
+
 }]);
 
 
