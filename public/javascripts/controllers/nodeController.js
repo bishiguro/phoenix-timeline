7dd50887524be13e7453705291484bb2a3a3c6c7
manickyoj <hotshot354@gmail.com>
2015-04-13 21:52:09 -0400

    Moved to the angular-ui-bootstrap framework and fixed add-node menu to actually add-node.

--- public/javascripts/controllers/nodeController.js
+++ public/javascripts/controllers/nodeController.js
@@ -1,5 +1,5 @@
-
-var app = angular.module('projectManager', []).controller('nodeController', ['$scope', '$http', function($scope,$http) {
+angular.module('projectManager', ['ui.bootstrap']);
+angular.module('projectManager').controller('nodeController', ['$scope', '$http', function($scope,$http) {
     $scope.summary = '';
     $scope.description = '';
     $scope.dueDate = '';
@@ -8,14 +8,22 @@ var app = angular.module('projectManager', []).controller('nodeController', ['$s
     $scope.addNode = function() {
         $scope.visible = !$scope.visible;
         $http.post('/node/add',{sum:$scope.summary,desc:$scope.description,due:$scope.dueDate}).success(function(data,status,headers,config) {
-                $("#node-container").prepend("<button class='node' id='"+data.id+"'></button>");
-            }).error(console.error);
+            $("#node-container").prepend("<button class='node' id='"+data.id+"'></button>");
+        }).error(console.error);
     };
 
     $scope.show = function() {
         $scope.visible = !$scope.visible;
     };
 
+    $scope.status = {
+        isopen: false
+    };
+    
+    $scope.toggleDropdown = function($event) {
+        $event.preventDefault();
+        $event.stopPropagation();
+        $scope.status.isopen = !$scope.status.isopen;
+    };
+
 }]);
-
-
