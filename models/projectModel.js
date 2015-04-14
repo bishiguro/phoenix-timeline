4b6ef69ea2d1224edde9220ef50a6c0e106e11e4
Bonnie Ishiguro <bgishiguro@gmail.com>
2015-04-14 00:16:07 -0400

    Updated Project Model

--- models/projectModel.js
+++ models/projectModel.js
@@ -3,7 +3,7 @@ var mongoose = require("mongoose");
 var projectSchema = mongoose.Schema({
 	name: String,
 	streams: [{type: Schema.Types.ObjectId, ref: 'Stream'}]
-	//users: [{type: Schema.Types.ObjectId, ref: 'Project'}]
+	//users: [{type: Schema.Types.ObjectId, ref: 'User'}]
 });
 
 module.exports = mongoose.model('Project', projectSchema);
