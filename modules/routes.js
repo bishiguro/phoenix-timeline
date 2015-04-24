da886646c81f4de5a45cfe985a2b2bf8bc1bf131
Bonnie Ishiguro <bgishiguro@gmail.com>
2015-04-24 16:32:48 -0400

    Removed Debug Commenting

--- modules/routes.js
+++ modules/routes.js
@@ -27,7 +27,6 @@ function databaseError(err, req, res) {
 // ----- GET HANDLERS ----- //
 
 routes.home = function(req, res) {
-    console.log('Home Page!')
     res.sendFile(path.join(__dirname, '../views/index.html'));
 }
 
