971d05a93df30011d4fca43b369f7ea2a57feac3
tchoetso <tchoetso920@gmail.com>
2015-04-26 00:39:09 -0400

    adding date picker

--- public/javascripts/controllers.js
+++ public/javascripts/controllers.js
@@ -59,6 +59,60 @@ function ProjectCtrl ($scope, $http, $routeParams) {
     };
 }
 
+
+// date picker
+app.controller('datepickerController', ['$scope', '$http', function($scope,$http) {
+    // DATE PICKER //
+    $scope.today = function() {
+        $scope.dt = new Date();
+    };
+
+    $scope.today();
+
+    $scope.clear = function () {
+        $scope.dt = null;
+    };
+
+
+    $scope.open = function($event) {
+        $event.preventDefault();
+        $event.stopPropagation();
+
+        $scope.opened = true;
+    };
+
+    $scope.dateOptions = {
+        formatYear: 'yy',
+        startingDay: 1
+    };
+
+    $scope.formats = ['dd-MMMM-yyyy', 'yyyy/MM/dd', 'dd.MM.yyyy', 'shortDate'];
+    $scope.format = $scope.formats[0];
+
+
+    // TIME PICKER //
+    $scope.mytime = new Date();
+
+    $scope.hstep = 1;
+    $scope.mstep = 1;
+
+    $scope.options = {
+        hstep: [1, 2, 3],
+        mstep: [1, 5, 10, 15, 25, 30]
+    };
+
+    $scope.ismeridian = true;
+    $scope.toggleMode = function() {
+        $scope.ismeridian = ! $scope.ismeridian;
+    };
+
+    $scope.update = function() {
+        var d = new Date();
+        d.setHours( 14 );
+        d.setMinutes( 0 );
+        $scope.mytime = d;
+    };
+}]);
 // ----- Export Controllers
 app.controller('UserCtrl', ['$scope', '$http', '$location', '$modal', UserCtrl]);
 app.controller('ProjectCtrl', ['$scope', '$http', '$routeParams', ProjectCtrl]);
