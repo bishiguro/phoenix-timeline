e4ffe4fa1efbfa23bc6312d64cdf6f3f6b362116
Haley <haley.pelletier@students.olin.edu>
2015-05-01 15:37:03 -0400

    ability to update stream information

--- public/javascripts/controllers.js
+++ public/javascripts/controllers.js
@@ -75,6 +75,50 @@ function ProjectCtrl ($scope, $http, $routeParams) {
     }
 }
 
+//Controller for Stream Details Menu
+function StreamDetailsCtrl($scope, $http) {
+    $scope.streamValues = {
+        name: '',
+        beginning: '',
+        end: '',
+        users: []
+    };
+    $scope.status = {
+        displaying: false,
+        editing: false
+    };
+
+    // Toggle Edit Menu //
+    $scope.showStreamDetails = function(id,$event) {
+        // Get the stream's details from the backend & display them on the frontend
+        $http.get('/streams/'+id).success(function(data,status,headers,config) {
+            $event.preventDefault();
+            $event.stopPropagation();
+            $scope.streamValues.name = data.stream.name;
+            $scope.streamValues.beginning = data.stream.beginning;
+            $scope.streamValues.end = data.stream.end;
+            $scope.streamValues.users = data.stream.users;
+            $scope.status.displaying = !$scope.displaying;
+        }).error(console.error);
+    };
+
+    // Save an Edited Stream's Details //
+    $scope.saveEditedStream = function(stream) {
+        // Update model on the backend
+        $http.put('/streams/'+stream._id, {
+            name: $scope.streamValues.name,
+            beginning: $scope.streamValues.beginning,
+            end: $scope.streamValues.end
+        })
+        .success(function(data,status,headers,config) {
+            // Show the new name on the frontend
+            stream.name = $scope.streamValues.name;
+        }).error(console.error);
+    };
+};
+
+
 // ----- Export Controllers
 app.controller('UserCtrl', ['$scope', '$http', '$location', '$modal', UserCtrl]);
 app.controller('ProjectCtrl', ['$scope', '$http', '$routeParams', ProjectCtrl]);
+app.controller('StreamDetailsCtrl', ['$scope','$http', StreamDetailsCtrl]);
