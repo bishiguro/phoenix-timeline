68b0de9b495457334f736c016902a59f8b90da95
Haley <haley.pelletier@students.olin.edu>
2015-05-01 16:29:47 -0400

    moved node and event controllers into new file

--- public/javascripts/controllers.js
+++ public/javascripts/controllers.js
@@ -111,15 +111,106 @@ function StreamDetailsCtrl($scope, $http) {
 
     // Delete a Stream //
     $scope.deleteStream = function (id) {
-        // Delete it on the backend
         $http.delete('/streams/'+id)
             .success( function(data, status) {
-                //Remove it on the frontend
-                
             }).error(function(data, status){
         });
     }
 };
+
+function EventDetailsCtrl($scope, $http) {
+    $scope.eventValues = {
+        ttl: '',
+        start: '',
+        end: ''
+    };
+    $scope.status = {
+        displaying: false,
+    };
+
+    // Toggle Edit Menu //
+    $scope.showEventDetails = function(id,$event) {
+        $http.get('/events/'+id).success(function(data,status,headers,config) {
+            $event.preventDefault();
+            $event.stopPropagation();
+            $scope.eventValues.ttl = data.event.title;
+            $scope.eventValues.start = data.event.startTime;
+            $scope.eventValues.end = data.event.endTime;
+            $scope.status.displaying = !$scope.displaying;
+        }).error(console.error);
+    };
+
+    // Save an Edited Event's Details //
+    $scope.saveEditedEvent = function(event) {
+        // Update model on the backend
+        $http.put('/events/'+event._id, {
+            title: $scope.eventValues.ttl,
+            startTime: $scope.eventValues.start,
+            endTime: $scope.eventValues.end
+        })
+        .success(function(data,status,headers,config) {
+            // Show the new name on the frontend
+            event.title = $scope.eventValues.ttl;
+        })
+        .error(console.error);
+    };
+
+    // Delete an Event //
+    $scope.deleteStream = function (id) {
+        $http.delete('/events/'+id)
+            .success( function(data, status) {                
+            }).error(function(data, status){
+        });
+    }
+}
+
+function NodeDetailsCtrl($scope, $http) {
+    $scope.nodeValues = {
+        sum: '',
+        desc: '',
+        due: ''
+    };
+    $scope.status = {
+        displaying: false,
+    };
+
+    // Toggle Edit Menu //
+    $scope.showNodeDetails = function(id,$event) {
+        $http.get('/nodes/'+id)
+        .success(function(data,status,headers,config) {
+            $event.preventDefault();
+            $event.stopPropagation();
+            $scope.nodeValues.sum = data.node.summary;
+            $scope.nodeValues.desc = data.node.description;
+            $scope.nodeValues.due = dateFormat(data.node.dueDate,"m/dd/yy");
+            $scope.status.displaying = !$scope.status.displaying;
+        })
+        .error(console.error);
+    };
+
+    // Save an Edited Node's Details //
+    $scope.saveEditedNode = function(node) {
+        // Update model on the backend
+        $http.put('/nodes/'+id, {
+            summary: $scope.nodeValues.sum,
+            description: $scope.nodeValues.desc,
+            dueDate: $scope.nodeValues.due
+        })
+        .success(function(data,status,headers,config) {
+            // Show the new name on the frontend
+            node.summary = $scope.nodeValues.sum;
+        })
+        .error(console.error);
+    };
+
+    // Delete an Event //
+    $scope.deleteStream = function (id) {
+        $http.delete('/nodes/'+id)
+            .success( function(data, status) {                
+            }).error(function(data, status){
+        });
+    }
+}
 
 
 // Project Controller Modal Instance Control
@@ -139,3 +230,5 @@ app.controller('StreamCreationCtrl', function ($scope, $modalInstance) {
 app.controller('UserCtrl', ['$scope', '$http', '$location', '$modal', UserCtrl]);
 app.controller('ProjectCtrl', ['$scope', '$http', '$routeParams', '$modal', ProjectCtrl]);
 app.controller('StreamDetailsCtrl', ['$scope','$http', StreamDetailsCtrl]);
+app.controller('EventDetailsCtrl', ['$scope','$http', EventDetailsCtrl]);
+app.controller('NodeDetailsCtrl', ['$scope','$http', NodeDetailsCtrl]);
