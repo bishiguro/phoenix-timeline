493b65b3d0f9f0e1321a1be445fd273212bde864
manickyoj <hotshot354@gmail.com>
2015-05-10 14:02:09 -0400

    Removed unused 'blur' function from controllers file.

--- public/javascripts/controllers.js
+++ public/javascripts/controllers.js
@@ -290,8 +290,6 @@ function StreamCtrl($scope, $http, $modal) {
         });
     };
 
-    $scope.blur = function (event) { event.target.blur(); }
-
 
     // -- Save Edits -- //
     $scope.save = function () {
